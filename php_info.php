773fc580ae3035eb8b4d240d3877c871fdd95100
Carbonio16 <153811262+Carbonio16@users.noreply.github.com>
2024-05-15 10:38:41 +0000

    hello PHP

---
+++ php_info.php
@@ -0,0 +1,14 @@
+<?php
+
+    for ($x = 0; $x <= 10; $x++) {
+        echo "Ciao \n";
+    }
+
+    echo "Ecco qua un array associativo di Dollaro_SERVER -> ";
+
+    foreach ($_SERVER as $key => $value) { 
+        echo "$key : $value " . "\n";
+    }
+
+
+?>
